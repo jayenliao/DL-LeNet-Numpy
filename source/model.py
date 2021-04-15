c42a4de21be2a946303442ab2ab190e0aae68efa
Jay Liao <jay.chiehen@gmail.com>
2021-04-16 01:37:50 +0800

    Update model.py

--- source/model.py
+++ source/model.py
@@ -6,150 +6,7 @@ from source.layers import *
 from source.utils import *
 from source.utils import cp2np
 
-class DeepConvNet:
-    def __init__(self, input_dim,
-                 conv_param_1 = {'filter_num':16, 'filter_size':3, 'pad':1, 'stride':1},
-                 conv_param_2 = {'filter_num':16, 'filter_size':3, 'pad':1, 'stride':1},
-                 conv_param_3 = {'filter_num':32, 'filter_size':3, 'pad':1, 'stride':1},
-                 #conv_param_4 = {'filter_num':32, 'filter_size':3, 'pad':2, 'stride':1},
-                 #conv_param_5 = {'filter_num':64, 'filter_size':3, 'pad':1, 'stride':1},
-                 #conv_param_6 = {'filter_num':64, 'filter_size':3, 'pad':1, 'stride':1},
-                 hidden_size=50, output_size=50):
-
-        pre_node_nums = np.array([3*3*3, 16*3*3, 16*3*3, 32*32*32, hidden_size])
-        weight_init_scales = np.sqrt(2.0 / pre_node_nums) 
-        
-        print(pre_node_nums)
-
-        self.params = {}
-        pre_channel_num = input_dim[0]
-        for idx, conv_param in enumerate([conv_param_1, conv_param_2, conv_param_3]):
-            self.params['W' + str(idx+1)] = weight_init_scales[idx] * np.random.randn(conv_param['filter_num'], pre_channel_num, conv_param['filter_size'], conv_param['filter_size'])
-            self.params['b' + str(idx+1)] = np.zeros(conv_param['filter_num'])
-            pre_channel_num = conv_param['filter_num']
-        self.params['W4'] = weight_init_scales[3] * np.random.randn(32*32*32, hidden_size)
-        self.params['b4'] = np.zeros(hidden_size)
-        self.params['W5'] = weight_init_scales[4] * np.random.randn(hidden_size, output_size)
-        self.params['b5'] = np.zeros(output_size)
-
-        print(self.params.keys())
-        #for k in self.params.keys():
-        #    print(self.params[k].shape)
-
-        # レイヤの生成===========
-        self.layers, self.layer_names = [], []
-        
-        self.layers.append(Convolution(self.params['W1'], self.params['b1'],  # 0
-                           conv_param_1['stride'], conv_param_1['pad']))
-        self.layer_names.append('conv')
-        
-        self.layers.append(ReLU())                                            # 1
-        self.layer_names.append('relu')
-        
-        self.layers.append(Convolution(self.params['W2'], self.params['b2'],  # 2
-                           conv_param_2['stride'], conv_param_2['pad']))
-        self.layer_names.append('conv')
-        
-        self.layers.append(ReLU())                                            # 3
-        self.layer_names.append('relu')
-        
-        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 4
-        self.layer_names.append('pool')
-        
-        self.layers.append(Convolution(self.params['W3'], self.params['b3'],  # 5
-                           conv_param_3['stride'], conv_param_3['pad']))
-        self.layer_names.append('conv')
-        
-        self.layers.append(ReLU())                                            # 6
-        self.layer_names.append('relu')
-        
-        '''
-        self.layers.append(Convolution(self.params['W4'], self.params['b4'],  # 7
-                           conv_param_4['stride'], conv_param_4['pad']))
-        self.layers.append(ReLU())                                            # 8
-        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 9
-        self.layers.append(Convolution(self.params['W5'], self.params['b5'],  # 10
-                           conv_param_5['stride'], conv_param_5['pad']))
-        self.layers.append(ReLU())                                            # 11
-        self.layers.append(Convolution(self.params['W6'], self.params['b6'],  # 12
-                           conv_param_6['stride'], conv_param_6['pad']))
-        self.layers.append(ReLU())                                            # 13
-        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 14
-        '''
-        self.layers.append(Affine(self.params['W4'], self.params['b4']))      # 15
-        self.layer_names.append('linear')
-        
-        self.layers.append(ReLU())                                            # 16
-        self.layer_names.append('relu')
-        
-        self.layers.append(Affine(self.params['W5'], self.params['b5']))      # 17
-        self.layer_names.append('linear')
-        
-        self.last_layer = SoftmaxWithCrossEntropyLoss()
-
-    def predict(self, x, train_flg=False):
-        for layer in self.layers:
-            x = layer.forward(x)
-        return x
-
-    def loss(self, x, t):
-        y = self.predict(x, train_flg=True)
-        return self.last_layer.forward(y, t)
-
-    def accuracy_score(self, x, yt, top=1):
-        if yt.ndim != 1:
-            yt = np.argmax(yt, axis=1)
-        y = self.predict(x)
-        if top == 1:
-            y = np.argmax(y, axis=1)
-            acc = np.array(y == yt).mean()
-        else:
-            y = np.argsort(y, axis=1)[:,-top:]
-            lst = []
-            for i in range(len(yt)):
-                lst.append(yt[i] in y[i,:])
-            acc = np.array(lst).mean()
-        return cp2np(acc)
-
-    def gradient(self, x, t):
-        # forward
-        self.loss(x, t)
-
-        # backward
-        dout = 1
-        dout = self.last_layer.backward(dout)
-
-        tmp_layers = self.layers.copy()
-        tmp_layers.reverse()
-        for layer in tmp_layers:
-            dout = layer.backward(dout)
-
-        #for i, layer_idx in enumerate((0, 2, 5, 7, 10, 12, 15, 17)):
-        grads, params_i = {}, 1
-        for layer_i, layer_name in enumerate(self.layer_names):
-            if layer_name in ['conv', 'linear']:
-                grads['W' + str(params_i)] = self.layers[layer_i].dW
-                grads['b' + str(params_i)] = self.layers[layer_i].db
-                params_i += 1
-        return grads
-
-    def save_params(self, file_name="params.pkl"):
-        params = {}
-        for key, val in self.params.items():
-            params[key] = val
-        with open(file_name, 'wb') as f:
-            pickle.dump(params, f)
-
-    def load_params(self, file_name="params.pkl"):
-        with open(file_name, 'rb') as f:
-            params = pickle.load(f)
-        for key, val in params.items():
-            self.params[key] = val
-
-        for i, layer_idx in enumerate((0, 2, 5, 7, 10, 12, 15, 17)):
-            self.layers[layer_idx].W = self.params['W' + str(i+1)]
-            self.layers[layer_idx].b = self.params['b' + str(i+1)]
-
+# The main model
 
 class LeNet5:
     '''
@@ -322,3 +179,145 @@ class LeNet5:
                 self.layers[layer_i].W = self.params['W' + str(params_i)].dW
                 self.layers[layer_i].b = self.params['b' + str(params_i)].db
                 params_i += 1
+
+                
+# For debugging
+
+class DeepConvNet:
+    def __init__(self, input_dim,
+                 conv_param_1 = {'filter_num':16, 'filter_size':3, 'pad':1, 'stride':1},
+                 conv_param_2 = {'filter_num':16, 'filter_size':3, 'pad':1, 'stride':1},
+                 conv_param_3 = {'filter_num':32, 'filter_size':3, 'pad':1, 'stride':1},
+                 #conv_param_4 = {'filter_num':32, 'filter_size':3, 'pad':2, 'stride':1},
+                 #conv_param_5 = {'filter_num':64, 'filter_size':3, 'pad':1, 'stride':1},
+                 #conv_param_6 = {'filter_num':64, 'filter_size':3, 'pad':1, 'stride':1},
+                 hidden_size=50, output_size=50):
+
+        pre_node_nums = np.array([3*3*3, 16*3*3, 16*3*3, 32*32*32, hidden_size])
+        weight_init_scales = np.sqrt(2.0 / pre_node_nums) 
+        
+        print(pre_node_nums)
+
+        self.params = {}
+        pre_channel_num = input_dim[0]
+        for idx, conv_param in enumerate([conv_param_1, conv_param_2, conv_param_3]):
+            self.params['W' + str(idx+1)] = weight_init_scales[idx] * np.random.randn(conv_param['filter_num'], pre_channel_num, conv_param['filter_size'], conv_param['filter_size'])
+            self.params['b' + str(idx+1)] = np.zeros(conv_param['filter_num'])
+            pre_channel_num = conv_param['filter_num']
+        self.params['W4'] = weight_init_scales[3] * np.random.randn(32*32*32, hidden_size)
+        self.params['b4'] = np.zeros(hidden_size)
+        self.params['W5'] = weight_init_scales[4] * np.random.randn(hidden_size, output_size)
+        self.params['b5'] = np.zeros(output_size)
+
+        self.layers, self.layer_names = [], []
+        
+        self.layers.append(Convolution(self.params['W1'], self.params['b1'],  # 0
+                           conv_param_1['stride'], conv_param_1['pad']))
+        self.layer_names.append('conv')
+        
+        self.layers.append(ReLU())                                            # 1
+        self.layer_names.append('relu')
+        
+        self.layers.append(Convolution(self.params['W2'], self.params['b2'],  # 2
+                           conv_param_2['stride'], conv_param_2['pad']))
+        self.layer_names.append('conv')
+        
+        self.layers.append(ReLU())                                            # 3
+        self.layer_names.append('relu')
+        
+        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 4
+        self.layer_names.append('pool')
+        
+        self.layers.append(Convolution(self.params['W3'], self.params['b3'],  # 5
+                           conv_param_3['stride'], conv_param_3['pad']))
+        self.layer_names.append('conv')
+        
+        self.layers.append(ReLU())                                            # 6
+        self.layer_names.append('relu')
+        
+        '''
+        self.layers.append(Convolution(self.params['W4'], self.params['b4'],  # 7
+                           conv_param_4['stride'], conv_param_4['pad']))
+        self.layers.append(ReLU())                                            # 8
+        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 9
+        self.layers.append(Convolution(self.params['W5'], self.params['b5'],  # 10
+                           conv_param_5['stride'], conv_param_5['pad']))
+        self.layers.append(ReLU())                                            # 11
+        self.layers.append(Convolution(self.params['W6'], self.params['b6'],  # 12
+                           conv_param_6['stride'], conv_param_6['pad']))
+        self.layers.append(ReLU())                                            # 13
+        self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))             # 14
+        '''
+        self.layers.append(Affine(self.params['W4'], self.params['b4']))      # 15
+        self.layer_names.append('linear')
+        
+        self.layers.append(ReLU())                                            # 16
+        self.layer_names.append('relu')
+        
+        self.layers.append(Affine(self.params['W5'], self.params['b5']))      # 17
+        self.layer_names.append('linear')
+        
+        self.last_layer = SoftmaxWithCrossEntropyLoss()
+
+    def predict(self, x, train_flg=False):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x, train_flg=True)
+        return self.last_layer.forward(y, t)
+
+    def accuracy_score(self, x, yt, top=1):
+        if yt.ndim != 1:
+            yt = np.argmax(yt, axis=1)
+        y = self.predict(x)
+        if top == 1:
+            y = np.argmax(y, axis=1)
+            acc = np.array(y == yt).mean()
+        else:
+            y = np.argsort(y, axis=1)[:,-top:]
+            lst = []
+            for i in range(len(yt)):
+                lst.append(yt[i] in y[i,:])
+            acc = np.array(lst).mean()
+        return cp2np(acc)
+
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        tmp_layers = self.layers.copy()
+        tmp_layers.reverse()
+        for layer in tmp_layers:
+            dout = layer.backward(dout)
+
+        #for i, layer_idx in enumerate((0, 2, 5, 7, 10, 12, 15, 17)):
+        grads, params_i = {}, 1
+        for layer_i, layer_name in enumerate(self.layer_names):
+            if layer_name in ['conv', 'linear']:
+                grads['W' + str(params_i)] = self.layers[layer_i].dW
+                grads['b' + str(params_i)] = self.layers[layer_i].db
+                params_i += 1
+        return grads
+
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, layer_idx in enumerate((0, 2, 5, 7, 10, 12, 15, 17)):
+            self.layers[layer_idx].W = self.params['W' + str(i+1)]
+            self.layers[layer_idx].b = self.params['b' + str(i+1)]
